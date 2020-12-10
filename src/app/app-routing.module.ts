f28c774f3840f6aa596e82a0c86da9fa414ff6d9
Mahmoud Skafi <mahmoudskafi277@gmail.com>
2020-12-10 19:07:26 +0200

    create: 1-login component 2-register component 3-home component 4-notfound component 🗂️

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
